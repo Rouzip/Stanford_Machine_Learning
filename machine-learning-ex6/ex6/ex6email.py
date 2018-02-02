7bc85541153368672709d474335ea999d737e2a3
Rouzip <1226015390@qq.com>
2018-02-02 23:50:26 +0800

    start svm part2

---
+++ machine-learning-ex6/ex6/ex6email.py
@@ -0,0 +1,12 @@
+from sklearn.svm import SVC
+from scipy import io
+import numpy as np
+
+
+def load_txt(filename):
+    with open(filename, 'r') as fp:
+        data = fp.read()
+
+
+if __name__ == '__main__':
+    pass
