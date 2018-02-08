745377f6a727dda3c7b2a47457e76edfb2686661
Rouzip <1226015390@qq.com>
2018-02-08 15:29:18 +0800

    Kmeans complete

--- machine-learning-ex7/ex7/ex7.py
+++ machine-learning-ex7/ex7/ex7.py
@@ -2,6 +2,7 @@ import numpy as np
 from scipy import io
 import matplotlib.pyplot as plt
 from matplotlib import colors
+import cv2
 
 
 def find_closest_centroids(X, initial_centroids):
@@ -26,38 +27,55 @@ def compute_centroids(X, idx, K):
     return centroids
 
 
-def run_kmeans(X, initial_centroids, max_iters, plot_progress):
+def run_Kmeans(X, initial_centroids, max_iters, plot_progress=False):
     m, n = X.shape
     K = initial_centroids.shape[0]
     centroids = initial_centroids
     previous_centroids = centroids
-    idx = np.zeros((m, 1))
     for i in range(max_iters):
-        print('K-Means iteration {}/{}...\n'.format(i, max_iters))
+        print('K-Means iteration {}/{}...\n'.format(i + 1, max_iters))
+        # 计算每个点属于哪个类 idx:[m,1]
         idx = find_closest_centroids(X, centroids)
         if plot_progress:
-            plt = plot_process_kmeans(X, centroids, previous_centroids)
+            plt = plot_process_Kmeans(X, centroids, previous_centroids, idx, K)
             previous_centroids = centroids
+            plt.show()
         centroids = compute_centroids(X, idx, K)
-    if plot_progress:
-        plt.show()
     return centroids, idx
 
 
-def plot_process_kmeans(X, centroids, previous_centroids):
+def plot_process_Kmeans(X, centroids, previous_centroids, idx, K):
     '''
     画出散点图，并画出中点变化的过程
     '''
-    plt.scatter(X[:, 0], X[:, 1])
+    plot_data_points(X, idx, K)
     plt.plot(previous_centroids[:, 0], previous_centroids[
-             :, 1], c='r', marker='x', markersize=10, linewidth=5)
-    plt.plot(centroids[:, 0], centroids[:, 1], c='r',
-             marker='x', markersize=10, linewidth=5)
+             :, 1], 'rx', markersize=10, linewidth=5)
+    plt.plot(centroids[:, 0], centroids[:, 1],
+             'rx', markersize=10, linewidth=5)
     for i in range(centroids.shape[0]):
         p1 = centroids[i, :]
         p2 = previous_centroids[i, :]
-        plt.plot(p1[0], p2[0], p1[1], p2[1], '->', linewidth=2)
+        plt.plot([p1[0], p2[0]], [p1[1], p2[1]], '->', linewidth=2)
     return plt
+
+
+def Kmeans_randinitial_centroids(X, K):
+    randidx = np.random.permutation(np.array(range(K)))
+    result = X[randidx, :]
+    return result
+
+
+def plot_data_points(X, idx, K):
+    '''
+    显示出调色出的散点图
+    '''
+    # inspired by http://stackoverflow.com/q/23945764/583834
+    palette = colors.hsv_to_rgb(
+        np.column_stack((np.linspace(0, 1, K + 1).reshape(K + 1, -1),
+                         np.ones((K + 1, 2)))))
+    colors_map = np.array([palette[int(i)] for i in idx])
+    plt.scatter(X[:, 0], X[:, 1], s=75, edgecolors=colors_map)
 
 
 if __name__ == '__main__':
@@ -67,10 +85,10 @@ if __name__ == '__main__':
     X = data['X']
     initial_centroids = np.array([[3, 3], [6, 2], [8, 5]])
     idx = find_closest_centroids(X, initial_centroids)
-    # print('Closest centroids for the first 3 examples: \n')
+    print('Closest centroids for the first 3 examples: \n')
     # 预期值为0,2,1
-    # print(idx[:3])
-    # input('next step')
+    print(idx[:3])
+    input('next step')
 
     # part2 compute means
     centroids = compute_centroids(X, idx, K)
@@ -82,4 +100,32 @@ if __name__ == '__main__':
     K = 3
     max_iter = 10
     initial_centroids = np.array([[3, 3], [6, 2], [8, 5]])
-    centroids, idx = run_kmeans(X, initial_centroids, max_iter, True)
+    centroids, idx = run_Kmeans(X, initial_centroids, max_iter, True)
+
+    # part4 K-Means clustering on pixels
+    img = cv2.imread('./bird_small.png')
+    img = img / 255
+    X = img.reshape(img.shape[0] * img.shape[1], 3)
+    K = 16
+    max_iter = 10
+    initial_centroids = Kmeans_randinitial_centroids(X, K)
+    centroids, _ = run_Kmeans(X, initial_centroids, max_iter)
+    input('next step')
+
+    # part5 Image Compression
+    idx = find_closest_centroids(X, centroids)
+    idx = np.array([int(i) for i in idx])
+    idx = idx.flatten()
+    X_recovered = centroids[idx, :]
+    X_recovered = X_recovered.reshape(img.shape[0], img.shape[1], 3)
+    # 交换通道，cv2和matplotlib默认显示通道不同
+    # cv2: r,g,b    matplotlib:b,g,r
+    r, g, b = cv2.split(X_recovered)
+    img_recover = cv2.merge([b, g, r])
+    r, g, b = cv2.split(img)
+    img = cv2.merge([b, g, r])
+    plt.subplot(1, 2, 1)
+    plt.imshow(img)
+    plt.subplot(1, 2, 2)
+    plt.imshow(img_recover)
+    plt.show()
